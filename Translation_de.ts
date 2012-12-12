a9a241eb543009d33cd4daf4bd695c3fb8ccb9fd
Marcel Schwittlick <marzzzel@gmail.com>
2012-12-12 19:41:42 +0100

    added translation

---
+++ Translation_de.ts
@@ -0,0 +1,346 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="28"/>
+        <source>File</source>
+        <translation type="unfinished">Datei</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>Edit</source>
+        <translation type="unfinished">Bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="51"/>
+        <source>Help</source>
+        <translation type="unfinished">Hilfe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="64"/>
+        <source>toolBar</source>
+        <translation type="unfinished">Werkzeugleiste</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="88"/>
+        <location filename="mainwindow.ui" line="91"/>
+        <source>Open</source>
+        <translation type="unfinished">Öffnen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="94"/>
+        <source>Opens a new Image</source>
+        <translation type="unfinished">Öffnet ein neues Bild</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="97"/>
+        <source>Ctrl+O</source>
+        <translation type="unfinished">Strg+O</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
+        <source>Save</source>
+        <translation type="unfinished">Speichern</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="109"/>
+        <source>Saves the current image</source>
+        <translation type="unfinished">Speichert das Bild</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <source>Ctrl+S</source>
+        <translation type="unfinished">Strg+S</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="117"/>
+        <source>Close</source>
+        <translation type="unfinished">Schliessen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="120"/>
+        <source>Closes the application</source>
+        <translation type="unfinished">Schließt die Anwendung</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="123"/>
+        <source>Ctrl+E</source>
+        <translation type="unfinished">Strg+E</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="132"/>
+        <source>Zoom in</source>
+        <translation type="unfinished">Heranzoomen </translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="135"/>
+        <source>Zoom into the image</source>
+        <translation type="unfinished">Zoomt an das Bild heran</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="138"/>
+        <source>Ctrl+]</source>
+        <translation type="unfinished">Strg++</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="147"/>
+        <source>Zoom Out</source>
+        <translation type="unfinished">Herauszoomen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="150"/>
+        <source>Zoom out of the image</source>
+        <translation type="unfinished">Zoomt aus dem Bild heraus</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="153"/>
+        <source>Ctrl+-</source>
+        <translation type="unfinished">Strg+-</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <source>Normal Size</source>
+        <translation type="unfinished">Normale Größe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="165"/>
+        <source>Restores the image to the normal size</source>
+        <translation type="unfinished">Stellt die normale Größe des Bilder wieder her</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="168"/>
+        <source>Ctrl+N</source>
+        <translation type="unfinished">Strg+N</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="173"/>
+        <source>About</source>
+        <translation type="unfinished">Über</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="178"/>
+        <source>About Qt</source>
+        <translation type="unfinished">Über Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="183"/>
+        <source>Subsampling</source>
+        <translation type="unfinished">Unterabststung</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="192"/>
+        <source>Save As</source>
+        <translation type="unfinished">Speichern Unter</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="197"/>
+        <source>Copy</source>
+        <translation type="unfinished">Kopieren</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="200"/>
+        <source>Ctrl+C</source>
+        <translation type="unfinished">Strg+C</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="205"/>
+        <source>Paste</source>
+        <translation type="unfinished">Einfügen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="208"/>
+        <source>Ctrl+V</source>
+        <translation type="unfinished">Strg+V</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="15"/>
+        <source>EMS</source>
+        <translation type="unfinished">EMS</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="28"/>
+        <source>Programm started.</source>
+        <translation type="unfinished">Programm gestartet.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <source>Open File</source>
+        <translation type="unfinished">Datei öffnen</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <source>Images</source>
+        <translation type="unfinished">Bilder</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="55"/>
+        <source>All Files</source>
+        <translation type="unfinished">Alle Dateien</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="60"/>
+        <location filename="mainwindow.cpp" line="201"/>
+        <source>Image Viewer</source>
+        <translation type="unfinished">Bildbetrachtung</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="60"/>
+        <location filename="mainwindow.cpp" line="201"/>
+        <source>Could not open the file %1.</source>
+        <translation type="unfinished">Konnte folgende Datei nicht öffnen %1.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="61"/>
+        <location filename="mainwindow.cpp" line="202"/>
+        <source>Could not open File.</source>
+        <translation type="unfinished">Konnte Datei nicht öffnen.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="66"/>
+        <location filename="mainwindow.cpp" line="207"/>
+        <source>Image successfully loaded.</source>
+        <translation type="unfinished">Das Bild wurde erfolgreich geöffnet.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="79"/>
+        <location filename="mainwindow.cpp" line="94"/>
+        <source>Image successfully saved.</source>
+        <translation type="unfinished">Das Bild wurde erfolgreich gespeichert.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="84"/>
+        <location filename="mainwindow.cpp" line="99"/>
+        <source>Image could not be saved.</source>
+        <translation type="unfinished">Das Bild konnte nicht gespeichert werden.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="89"/>
+        <source>Save File</source>
+        <translation type="unfinished">Datei Speichern</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="89"/>
+        <source>PNG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="89"/>
+        <source>JPEG</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="89"/>
+        <source>BMP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="128"/>
+        <source>Image restored to original size.</source>
+        <translation type="unfinished">Das Bild wurde auf Originalgröße zurückgesetzt.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="136"/>
+        <source>About Belegarbeit</source>
+        <translation type="unfinished">Über Belegarbeit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="136"/>
+        <source>Members: 
+Marcel Schwittlick (s0529494) 
+Alexander Marten (s0527574)
+Dennis Hägler (s0529999)</source>
+        <translation type="unfinished">Mitglieder:
+Marcel Schwittlick (s0529494) 
+Alexander Marten (s0527574)
+Dennis Hägler (s05232338)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="166"/>
+        <source>Copied image to clipboard.</source>
+        <translation type="unfinished">Das bild wurde von der Zwischenablage kopiert.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="180"/>
+        <location filename="mainwindow.cpp" line="191"/>
+        <source>Pasting from clipboard succeded.</source>
+        <translation type="unfinished">Das Einfügen aus der Zwischenablage war erfolgreich.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="182"/>
+        <location filename="mainwindow.cpp" line="188"/>
+        <source>Pasting from clipboard failed.</source>
+        <translation type="unfinished">Das Einfügen aus der Zwischenablage schlug fehl.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="281"/>
+        <source>&amp;%1 %2</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MyGraphicsView</name>
+    <message>
+        <location filename="mygraphicsview.cpp" line="204"/>
+        <source>Image sucessfully loaded.</source>
+        <translation type="unfinished">Das Bild wurde erfolgreich geladen.</translation>
+    </message>
+    <message>
+        <location filename="mygraphicsview.cpp" line="207"/>
+        <location filename="mygraphicsview.cpp" line="210"/>
+        <source>Image loading failed.</source>
+        <translation type="unfinished">Das Bild konnte nicht geladen werden.</translation>
+    </message>
+    <message>
+        <location filename="mygraphicsview.cpp" line="230"/>
+        <source>Image zoomed in.</source>
+        <translation type="unfinished">Herangezoomt.</translation>
+    </message>
+    <message>
+        <location filename="mygraphicsview.cpp" line="237"/>
+        <source>Image zoomed out.</source>
+        <translation type="unfinished">Herausgezoomt.</translation>
+    </message>
+</context>
+<context>
+    <name>MySliderDialog</name>
+    <message>
+        <location filename="sliderdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sliderdialog.ui" line="102"/>
+        <source>:1   R</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sliderdialog.ui" line="115"/>
+        <source>:1   G</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sliderdialog.ui" line="128"/>
+        <source>:1   B</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sliderdialog.ui" line="189"/>
+        <location filename="sliderdialog.ui" line="202"/>
+        <location filename="sliderdialog.ui" line="215"/>
+        <source>1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="sliderdialog.ui" line="228"/>
+        <source>Preview</source>
+        <translation type="unfinished">Vorschau</translation>
+    </message>
+</context>
+</TS>
